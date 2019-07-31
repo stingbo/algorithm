72b896bfe2099c2fbcbf252dcc456b0ba628f84a
万连波 <sting_bo@163.com>
2019-07-31 20:12:49 +0800

    增加bfs-maze算法

---
+++ maze.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * BFS找到最短路径.
+ */
+class BfsMaze
+{
+    public $start;
+
+    public $end;
+
+    public $path;
+
+    public function search()
+    {
+    }
+}
+
+$maze = [
+    [0, 1, 0, 0, 0],
+    [0, 1, 0, 1, 0],
+    [0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 0],
+    [0, 0, 0, 1, 0],
+];
