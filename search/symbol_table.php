2605f276bdee5e0f4e22cc053b1b78390d37be4a
万连波 <sting_bo@163.com>
2019-09-02 09:11:22 +0800

    symbol table

---
+++ search/symbol_table.php
@@ -0,0 +1,54 @@
+<?php
+
+class ST
+{
+    /**
+     * 将键值存入表中.
+     */
+    public function put($key, $val): void
+    {
+    }
+
+    /**
+     * 获取键对应的值.
+     */
+    public function get($key)
+    {
+        return 'aaaa';
+    }
+
+    /**
+     * 删除key对应的值.
+     */
+    public function delete($key): void
+    {
+    }
+
+    /**
+     * key在表中是否有对应的值.
+     */
+    public function contains($key): bool
+    {
+    }
+
+    /**
+     * 表是否为空.
+     */
+    public function isEmpty($key): bool
+    {
+    }
+
+    /**
+     * 表中的键值对数量.
+     */
+    public function size(): int
+    {
+    }
+
+    /**
+     * 所有键的集合.
+     */
+    public function keys(): array
+    {
+    }
+}
