2bc5a2d507932e0adb00ea67e97e2111885b6189
万连波 <sting_bo@163.com>
2019-09-18 21:18:59 +0800

    修改字母三角形

--- triangle.php
+++ triangle.php
@@ -5,16 +5,25 @@ $end = ord($argv[1]);
 if (!$end) {
     die('请转入一个大写字母');
 }
+$array = [];
 
-for ($i = $start - 1; $i < $end; $i++) {
+for ($i = $start; $i <= $end; $i++) {
     for ($j = $end; $j > $i; $j--) {
         echo ' ';
     }
     for ($k = $start; $k <= $i; $k++) {
         echo chr($k);
+        if ($k < $i) {
+            array_push($array, chr($k));
+        }
     }
-    for ($k = $i+1; $k >= $start; $k--) {
-        echo chr($k);
+
+    while ($array) {
+        echo array_pop($array);
     }
+
+    //for ($m = $i-1; $m >= $start; $m--) {
+        //echo chr($m);
+    //}
     echo "\n";
 }
